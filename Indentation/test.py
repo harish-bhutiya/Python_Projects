cc6fc4b08e5d2447702ee267997a9c51a2657fa3
harish-bhutiya <harishbhutiya23@gmail.com>
2025-06-18 11:43:41 +0100

    Initial commit

---
+++ Indentation/test.py
@@ -0,0 +1,17 @@
+#price comparison
+
+#products price
+ford = 28000
+
+toyota = 45000
+
+# check the product which is expensive
+
+if ford < toyota:
+    print("Toyota is expensive than ford mustang")
+
+elif ford > toyota:
+    print("Ford Mustang is expensive than toyota")
+
+else :
+    print("Both products have the same price")
